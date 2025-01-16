63bd71ee9c0f6dd2f3511128626994ee68e148ae
sahilmund.6685@gmail.com <sahilmund.6685@gmail.com>
2025-01-16 23:33:17 +0530

    added

---
+++ multiThreading/main.java
@@ -0,0 +1,10 @@
+ class Main{
+    public static void main(String[] args) {
+        // Thread lifecycle
+        world t1 = new world(); //NEW state
+        t1.start(); //RUNNABLE state
+        
+
+
+}
+}
